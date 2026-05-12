c56431d1b0e54f34a954357fa7f7164ecc145c16
华峻 <huajun@jmacmini.local>
2026-05-12 17:36:19 +0800

    Replace achievement trophy icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'back-safe-checkin-v20260512-achievement-icons-rail';
+const CACHE_NAME = 'back-safe-checkin-v20260512-achievement-medal';
 const APP_SHELL = [
   './',
   './index.html',
