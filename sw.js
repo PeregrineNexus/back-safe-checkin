8ee245c32c6c742ad9626d6657fd2b57fe3867ae
华峻 <huajun@jmacmini.local>
2026-05-12 17:19:02 +0800

    Mute inactive achievement badges

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'back-safe-checkin-v20260512-achievement-icons-light';
+const CACHE_NAME = 'back-safe-checkin-v20260512-achievement-icons-muted';
 const APP_SHELL = [
   './',
   './index.html',
