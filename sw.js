a26a2231718a93bca239d3c3e230e7d09f1d55d0
华峻 <huajun@jmacmini.local>
2026-05-12 16:48:11 +0800

    Improve achievement icon contrast

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'back-safe-checkin-v20260512-achievement-icons';
+const CACHE_NAME = 'back-safe-checkin-v20260512-achievement-icons-contrast';
 const APP_SHELL = [
   './',
   './index.html',
