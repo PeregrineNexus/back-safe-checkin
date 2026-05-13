66e6ccd3b4ff28ffe0d48ce0f92cced269ddbb7b
华峻 <huajun@jmacmini.local>
2026-05-13 16:04:13 +0800

    Load app images eagerly in scroll container

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'back-safe-checkin-v20260513-achievement-icon-cache-fix';
+const CACHE_NAME = 'back-safe-checkin-v20260513-eager-image-load';
 const APP_SHELL = [
   './',
   './index.html',
